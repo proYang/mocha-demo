a84bd3c66349989bda3d39e5073f5b53b934be48
SlaneYang <904509221@qq.com>
2017-08-08 09:58:57 +0800

    使用power-assert断言库

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
 const app = require('../src/app')
 const request = require('supertest');
 const should = require('should')
+const assert = require('power-assert')
 
 // 测试后端api
 describe('GET /user', function () {
@@ -12,6 +13,21 @@ describe('GET /user', function () {
             .expect(200)
             .end(function (err, res) {
                 res.body.name.should.be.equal('slane');
+                done();
+            });
+    });
+});
+
+// 断言库powerassert
+describe('GET /user', function () {
+    it('respond with json', function (done) {
+        request(app)
+            .get('/user')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                assert(res.body.name === 'slane')
                 done();
             });
     });
